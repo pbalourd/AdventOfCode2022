9cb1cf6b0f25b693997764d2926515c9b50a0725
Rainbow <pbalourd@yahoo.com>
2023-01-20 15:02:17 +0200

    Day25.

---
+++ src/main/kotlin/Day25.kt
@@ -0,0 +1,25 @@
+import java.io.File
+
+fun main() {
+    val snafuNumbers = File("src/main/kotlin/input25.txt").readLines()
+
+    println( longToSnafu( snafuNumbers.sumOf { snafuToLong(it) } ) )
+
+}
+
+fun snafuToLong(snafu: String): Long {
+    val snafuDigitsMap = mapOf('2' to 2L, '1' to 1, '0' to 0, '-' to -1, '=' to -2)
+    return snafu.fold(0L) { acc, ch -> acc * 5L + snafuDigitsMap[ch]!! }
+}
+
+fun longToSnafu(num: Long): String {
+    val decimalDigitsList = listOf('0', '1', '2', '=', '-')
+    var n = num
+    return buildString {
+        while (n > 0) {
+            append(decimalDigitsList[(n % 5).toInt()])
+            n = (n + 2) / 5
+        }
+    }.reversed()
+}
+
